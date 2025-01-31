dd0d4d2b7cc37b103657afa8de3d394f97efb056
Arif Khan <khanarif2738@gmail.com>
2025-02-01 00:22:28 +0530

    Add addToCart functionality in backend

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/mongodb.js"
 import connectCloudinary from "./config/cloudinary.js"
 import userRouter from "./routes/userRoute.js"
 import productRouter from "./routes/productRoute.js"
+import cartRouter from "./routes/cartRoute.js"
 
 // App Config
 const app = express()
@@ -21,6 +22,7 @@ app.use(cors())
 // api endpoints
 app.use('/api/user',userRouter)   // import userRouter
 app.use('/api/product',productRouter)   // import productRouter
+app.use('/api/cart',cartRouter)  // import cartRouter
 
 app.get('/', (req,res) => { 
     res.send("API Working")
